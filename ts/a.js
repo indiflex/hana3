2a8d00bb12c98a92ef7c2517ea673d56a8d6e717
indiflex <indiflex1@gmail.com>
2024-02-14 13:46:14 +0900

    combine

---
+++ ts/a.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var mmm_1 = require("mmm");
+var s = 1;
+var str = 'abc';
+console.log('🚀  str:', str.replaceAll('a', 'A'));
+var x;
+// let y: Y;
+var z;
+var zz;
+function add(a, b) {
+    return a + b;
+}
+add(1, 2);
+console.log('xx=', mmm_1.xx);
---
+++ ts/types/study.d.ts
@@ -0,0 +1,2 @@
+export type Y = string | number;
+
