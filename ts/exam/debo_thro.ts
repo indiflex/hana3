bc10039b342b65768f8e2e707bd3050057e3433e
indiflex <indiflex1@gmail.com>
2024-02-15 13:41:40 +0900

    rangeSum & keyPair

--- ts/exam/debo_thro.ts
+++ ts/exam/debo_thro.ts
@@ -1,7 +1,11 @@
 const debounce = (cb: (...args: unknown[]) => void, delay: number = 0) => {
-  let timer: ReturnType<typeof setTimeout>;
+  // let timer: ReturnType<typeof setTimeout>;
+  let timer: number;
   return (...args: unknown[]) => {
-    if (timer) clearTimeout(timer);
+    if (timer) {
+      console.log(timer);
+      clearTimeout(timer);
+    }
     timer = setTimeout(cb, delay, ...args);
   };
 };
@@ -25,3 +29,16 @@ for (let i = 10; i < 15; i += 1) debo(i); // 15
 
 const thro = throttle(a => console.log('thro=', a + 1), 1000);
 for (let i = 10; i < 15; i += 1) thro(i); // 11
+
+enum BTX {
+  A = 'A',
+  B = 'B',
+  AB = 'AB',
+  O = 'O',
+}
+
+function btype(bt: BTX) {
+  // if (bt === BTX.A) {
+  if (bt === 'A') {
+  }
+}
