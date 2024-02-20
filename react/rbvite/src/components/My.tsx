462acb8022d4658def88589f3445a074529a4fbf
indiflex <indiflex1@gmail.com>
2024-02-20 12:44:32 +0900

    addCartItem

--- react/rbvite/src/components/My.tsx
+++ react/rbvite/src/components/My.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import { Cart, Session } from '../App';
 import { Login } from './Login';
 import { Profile } from './Profile';
@@ -7,6 +8,7 @@ type Props = {
   login: (id: number, name: string) => void;
   logout: () => void;
   removeItem: (itemId: number) => void;
+  saveItem: (item: Cart) => void;
 };
 
 export const My = ({
@@ -14,8 +16,30 @@ export const My = ({
   login,
   logout,
   removeItem,
+  saveItem,
 }: Props) => {
+  const itemNameRef = useRef<HTMLInputElement>(null);
+  const itemPriceRef = useRef<HTMLInputElement>(null);
   // if (loginUser) loginUser.name = 'XXXXXXX';
+
+  const saveCartItem = (e: React.FormEvent) => {
+    e.preventDefault();
+    const name = itemNameRef.current?.value;
+    const price = Number(itemPriceRef.current?.value);
+    if (!name) {
+      alert('상품명을 정확히 입력하세요!');
+      itemNameRef.current?.focus();
+      return;
+    } else if (isNaN(price) || !price) {
+      alert('가격을 정확히 입력하세요!');
+      itemPriceRef.current?.focus();
+      return;
+    }
+
+    saveItem({ id: 0, name, price });
+    itemNameRef.current.value = '';
+    if (itemPriceRef.current) itemPriceRef.current.value = '0';
+  };
   return (
     <div
       style={{ border: '2px solid red', marginBottom: '2rem', padding: '1rem' }}
@@ -35,6 +59,11 @@ export const My = ({
           </li>
         ))}
       </ul>
+      <form onSubmit={saveCartItem}>
+        <input type='text' ref={itemNameRef} placeholder='상품명...' />
+        <input type='number' ref={itemPriceRef} placeholder='금액...' />
+        <button type='submit'>추가</button>
+      </form>
     </div>
   );
 };
