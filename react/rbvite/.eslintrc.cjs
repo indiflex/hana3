dcec0016977d7cc8e3207a5df7af22ae7c1860d9
indiflex <indiflex1@gmail.com>
2024-02-26 17:43:42 +0900

    react-router

--- react/rbvite/.eslintrc.cjs
+++ react/rbvite/.eslintrc.cjs
@@ -28,6 +28,6 @@ module.exports = {
     'prettier/prettier': 'error',
     quotes: ['error', 'single', { allowTemplateLiterals: true }],
     'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
   },
 };
