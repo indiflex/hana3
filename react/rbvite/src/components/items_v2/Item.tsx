d678745b0b3099710e233d37c4c75f4caa6ae5e9
indiflex <indiflex1@gmail.com>
2024-02-28 14:02:26 +0900

    ItemLayout - v2

--- react/rbvite/src/components/items_v2/Item.tsx
+++ react/rbvite/src/components/items_v2/Item.tsx
@@ -1,14 +1,16 @@
 import { useNavigate, useOutletContext, useParams } from 'react-router-dom';
 import { useSession } from '../../contexts/session-context';
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import { FaEdit } from 'react-icons/fa';
-import { Button } from '../Button';
+import { Button } from '../ui/Button';
 
 export const Item = () => {
   const {
     session: { cart },
   } = useSession();
   const [item, setItem] = useState<Cart>({ id: 0, name: '', price: 0 });
+  const [isEditing, toggleEditing] = useReducer((pre) => !pre, false);
+
   const { item: contextItem } = useOutletContext<{ item: Cart }>();
   const navigate = useNavigate();
   const { id } = useParams();
@@ -35,10 +37,16 @@ export const Item = () => {
       ) : (
         <>
           <h1 className='text-lg'>상품 정보</h1>
-          {item.name} ({item.price.toLocaleString()}원)
-          <Button type='primary'>
-            <FaEdit /> Edit
-          </Button>
+          <div>
+            {item.name} ({item.price.toLocaleString()}원)
+            <Button
+              onClick={() => toggleEditing()}
+              variant='primary'
+              className=''
+            >
+              <FaEdit /> Edit
+            </Button>
+          </div>
         </>
       )}
 
